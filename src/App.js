48eb90f6161440e5d3133804eb5838f40f477a95
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-06 12:30:03 +0200

    added logic to generate text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count); // input from form is a string, need to parse to int
+
+    // some small form of validation
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   }
 
   return (
@@ -27,7 +36,9 @@ function App() {
         <button className='btn' title="Generate">generate</button>
       </form>
       <article className='lorem-text'>
-        <p>Test Data</p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   )
